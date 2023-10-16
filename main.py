6c5151b8ee33457240970b7398f6e8530a7fec5f
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2023-10-16 11:28:48 +0200

    Update network

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     from network import railway_network_current
     railway_network = railway_network_current()
     from network import visualize_network
-    visualize_network(railway_network)
+
 
