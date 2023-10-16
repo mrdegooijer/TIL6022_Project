73ef32301c6700c31ea66c9f5c11c634a877006b
dwolfrat <d.e.b.wolfrat@student.tudelft.nl>
2023-10-16 12:34:48 +0200

    Aangepast zodat er sporen de verschillende richtingen op zijn. En begonnen met het netwerk in dit model implementeren

--- physical_elements.py
+++ physical_elements.py
@@ -4,11 +4,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#class STATION:
-#    def __init__(self, name,  location, platforms):
-#        self.name = name
-#        self.location = location
-#        self.platforms = platforms
+
+from network import railway_network_current
+railway_network = railway_network_current()
+from network import visualize_network
+graph = visualize_network(railway_network)
 
 
 class TRAIN:
@@ -19,17 +19,9 @@ class TRAIN:
         self.route = route
         self.speed_distance = speed_distance # speed in meter (per time step of 10s)
         self.time = 0
-#        self.acceleration = acceleration
-#        self.deceleration = deceleration
+
 
     def move(self, train_locations):
-
-        if self.location == "HS":
-            startlocation = "HS"
-        else:
-            start_locations = "R"
-# nu iets aan de code aan zien te passen waardoor de treinen van een bepaalde richting niet op hetzelfde spoor rijden als van een andere richting
-
         if self.route:
             next_station = self.route[0]
             distance_to_next_station = abs(next_station - self.location)
@@ -50,46 +42,59 @@ class TRAIN:
                 self.route.pop(0)
                 train_locations[self.train_id] = self.location
                 print(f"Train {self.train_id} arrived at {next_station} meters.")
-                #In deze situ hebben de stations onbeperkt peron capaciteit
+                #In deze situatie hebben de stations onbeperkt perron capaciteit
 
     def increase_time(self):
         self.time += 10  # Increase time with 10 seconds
 
 #hierin uiteindelijk de stations plaatsen
-route_HS_IC = [500, 800, 1000] # in m
+#route_HS_IC = [500, 800, 1000] # in m
+#route_HS_spr = [500, 800, 1000] # in m
+#route_R_IC = [1000, 800, 500] # in m
+#route_R_spr = [1000, 800, 500] # in m
+
+route_HS_IC = [railway_network.nodes["Den Haag HS"].get("pos")[1], railway_network.nodes["Delft"].get("pos")[1], railway_network.nodes["Rotterdam Centraal"].get("pos")[1]] # in m
 route_HS_spr = [500, 800, 1000] # in m
 route_R_IC = [500, 800, 1000] # in m
 route_R_spr = [500, 800, 1000] # in m
 
-# Constraint toevoegen waardoor er niet meerdere treinen op dezelfde locatie kunnen zijn
+print(railway_network.nodes["Den Haag HS"].get("pos")[1])
 
 #train data:
 start_locations = ["HS", "HS", "HS"] # start location
 traintype = ["IC", "IC", "IC"]
+
+#train_locations is used to keep track of the current location of each train. Furthermore the start location on the track is determined from train_locations
 train_locations = {0:0, 1:0, 2:0}
 
 
+
 def train_creator():
-    trains = []
+    trains_direction_1 = []
+    trains_direction_2 = []
 
     for train in range(len(start_locations)):
-        start_location = 0
         if start_locations[train] == "HS" and traintype[train] == "IC": # if the train from Den Haag HS to Rotterdam
             route = route_HS_IC
-            speed_distance = 100 #Waardes hiervoor nog aanpassen
+            speed = -100  # speed in meter (per time step of 10s)
+            trains_direction_1.append(TRAIN(train, traintype[train], train_locations[train], route, speed))
+
         elif start_locations[train] == "HS" and traintype[train] == "spr":
             route = route_HS_spr
-            speed_distance = 100 #Waardes hiervoor nog aanpassen
+            speed = -80  # speed in meter (per time step of 10s)
+            trains_direction_1.append(TRAIN(train, traintype[train], train_locations[train], route, speed))
+
         elif start_locations[train] == "R" and traintype[train] == "IC":
             route = route_R_IC
-            speed_distance = 100 #Waardes hiervoor nog aanpassen
+            speed = 100  # speed in meter (per time step of 10s)
+            trains_direction_2.append(TRAIN(train, traintype[train], train_locations[train], route, speed))
+
         else:
             route = route_R_spr
-            speed_distance = 100 #Waardes hiervoor nog aanpassen
+            speed = 80  # speed in meter (per time step of 10s)
+            trains_direction_2.append(TRAIN(train, traintype[train], train_locations[train], route, speed))
 
-        trains.append(TRAIN(train, traintype[train], start_location, route, speed_distance))
-
-    return trains
+    return [trains_direction_1, trains_direction_2]
 
 
 def run_simulation():
@@ -98,7 +103,11 @@ def run_simulation():
     trains = train_creator()
 
     for step in range(num_steps):
-        for train in trains:
+        for train in trains[0]:
+            train.move(train_locations)
+            train.increase_time()
+
+        for train in trains[1]:
             train.move(train_locations)
             train.increase_time()
 
