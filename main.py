d91a0d433c60b0b4e417b4183d493b9aa61dd45a
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2023-10-18 11:08:04 +0200

    Fixed the networkx again

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import networkx as nx
 if __name__ == '__main__':
     from network import railway_network_current
     railway_network = railway_network_current()
-    from network import visualize, visualize_network, visualize_2
+    from network import visualize, visualize_network, visualize_2, check_type
     #visualize(railway_network)
     #visualize_network(railway_network)
     visualize_2(railway_network)
+    #check_type(railway_network)
 
     # from physical_elements import TRAIN
     #
