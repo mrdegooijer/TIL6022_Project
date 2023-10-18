93b9763db29179997a6da93eacf3ad23c5eba573
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2023-10-18 12:41:52 +0200

    het werkt niet:(

--- ADDraw.py
+++ ADDraw.py
@@ -2,7 +2,14 @@ import networkx as nx
 import holoviews as hv
 
 def draw(network):
-    hv_graph = hv.Graph.from_networkx(network, nx.layout.spring_layout, k=0.5)
+    pos = nx.spring_layout(network, dim=2, k=0.5)
+    print(pos)
+
+    node_points = hv.Points(list(pos.values()))
+    node_points = node_points.redim(x = 'x', y = 'y')
+
+    hv_graph = hv.Graph.from_networkx(network, node_points)
+    graph_with_nodes = hv_graph * node_points
 
     hv_graph.opts(
         node_size=10,
@@ -22,8 +29,10 @@ def draw(network):
         title='Railway Network',
         fontsize={'title': 16, 'labels': 12, 'xticks': 8, 'yticks': 8}
     )
-
+    graph_with_nodes.show()
     hv.save(hv_graph, 'railway_network.html')
     hv_graph
 
     return
+
+
