5df0826e78349d625be5a9dd95c8fa7fc71f3d55
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2023-10-18 11:20:30 +0200

    draw()

---
+++ ADDraw.py
@@ -0,0 +1,29 @@
+pip install holoviews
+import holoviews as hv
+
+def draw(network):
+    hv_graph = hv.Graph.from_networkx(network, nx.layout.spring_layout, k=0.5)
+
+    hv_graph.opts(
+        node_size=10,
+        node_color='blue',
+        edge_color='black',
+        directed=True,
+        arrowhead_length=0.01,
+        arrowhead_angle=0,
+        width=800,
+        height=800,
+        padding=0.1,
+        xaxis=None,
+        yaxis=None,
+        show_frame=False,
+        show_grid=False,
+        show_legend=False,
+        title='Railway Network',
+        fontsize={'title': 16, 'labels': 12, 'xticks': 8, 'yticks': 8}
+    )
+
+    hv.save(hv_graph, 'railway_network.html')
+    hv_graph
+
+    return
--- main.py
+++ main.py
@@ -5,17 +5,10 @@ if __name__ == '__main__':
     from network import railway_network_current
     railway_network = railway_network_current()
     from network import visualize, visualize_network, visualize_2, check_type
-    #visualize(railway_network)
-    #visualize_network(railway_network)
-    visualize_2(railway_network)
-    #check_type(railway_network)
+    #visualize_2(railway_network)
 
-    # from physical_elements import TRAIN
-    #
-    # #create trains
-    # train1 = TRAIN(1, "IC", 0, [500, 800, 1000], 100)
-    #
-    # railway_network.add_node(train1.train_id, pos=(0, train1.location))
+    from ADDraw import draw
+    draw(railway_network)
 
 
 
