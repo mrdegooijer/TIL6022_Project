669ca182502e1e03ab22b31fd6eea398ae342337
Mischa de Gooijer <m.r.degooijer@student.tudelft.nl>
2023-10-30 12:07:49 +0100

    Small changes to the networkx visualization as a basis for the holoviews visualization

--- network.py
+++ network.py
@@ -22,7 +22,7 @@ def railway_network_current():
     railway_network.add_node("Switch 6", pos=(0, 21200), type="switch")
 
     # Add invisible nodes - platforms
-    railway_network.add_node("Rotterdam Centraal1", pos=(15, 0), type="platform")
+    railway_network.add_node("Rotterdam Centraal 1", pos=(15, 0), type="platform")
     railway_network.add_node("Rotterdam Centraal 2", pos=(5, 0), type="platform")
     railway_network.add_node("Rotterdam Centraal 3", pos=(-5, 0), type="platform")
     railway_network.add_node("Rotterdam Centraal 4", pos=(-15, 0), type="platform")
@@ -88,11 +88,12 @@ def railway_network_current():
     railway_network.add_edge("Switch 1 2", "Schiedam Centrum 2", length=1900, type="track2")
     railway_network.add_edge("Schiedam Centrum 1", "Switch 2 1", length=400, type="track1")
     railway_network.add_edge("Schiedam Centrum 2", "Switch 2 2", length=400, type="track2")
-    railway_network.add_edge("Switch 2 1", "Switch 3 1", length=6500, type="track1")
+    #railway_network.add_edge("Switch 2 1", "Switch 2 2", length=10, type="track1")
+    railway_network.add_edge("Switch 2 2", "Switch 3 1", length=6500, type="track1")
     railway_network.add_edge("Switch 3 1", "Delft Campus 1", length=1500, type="track1")
     railway_network.add_edge("Delft Campus 1", "Switch 4 1", length=300, type="track1")
     railway_network.add_edge("Switch 4 1", "Delft 1", length=1600, type="track1")
-    railway_network.add_edge("Delft 1", "Switch 5 1", length=2500, type="track1")
+    railway_network.add_edge("Delft 1", "Switch 5 2", length=2500, type="track1")
     railway_network.add_edge("Switch 5 1", "Rijswijk 1", length=1800, type="track1")
     railway_network.add_edge("Switch 5 2", "Rijswijk 2", length=1800, type="track2")
     railway_network.add_edge("Rijswijk 1", "Moerwijk 1", length=1900, type="track1")
@@ -201,7 +202,9 @@ def railway_network_future():
 
 def visualize_2(network):
     pos = nx.get_node_attributes(network, 'pos')
-    labels = {node: node if network.nodes[node]['type'] != 'switch' else '' for node in network.nodes}
+
+    empty_label = ['vswitch', 'platform']
+    labels = {node: node if network.nodes[node]['type'] not in empty_label else '' for node in network.nodes}
 
     # Define the figure size
     plt.figure(figsize=(6, 12))
@@ -212,125 +215,25 @@ def visualize_2(network):
     node_color = ['red' if network.nodes[node]['type'] == 'switch' else 'lightblue' for node in network.nodes]
     nx.draw_networkx_nodes(network, pos, node_size=node_size, node_color=node_color)
 
-    # Draw edges
+    #Create edgelist
+    edgelist = []
     for edge in network.edges(data=True):
-        if edge[2]['type'] == 'track':
-            nx.draw_networkx_edges(network, pos, edgelist=[(edge[0], edge[1])], edge_color='gray')
+        edgelist.append((edge[0], edge[1]))
+
+    #Draw edges
+    nx.draw_networkx_edges(network, pos, edgelist=edgelist, edge_color='gray', arrows=None)
 
     # Draw labels
-    nx.draw_networkx_labels(network, pos, labels=labels, font_size=10, font_color='black')
+    nx.draw_networkx_labels(network, pos, labels=labels, font_size=10, font_color='black', horizontalalignment='right')
 
     plt.title("Railway Network")
-    plt.axis('off')
+    plt.ylim(0, 22600)
+    plt.axis('auto')
 
     # Save the figure
     plt.savefig("railway_network.png")
 
     plt.show()
-
-
-def visualize_network(network):
-    pos = nx.get_node_attributes(network, 'pos')
-    labels = {node: node if network.nodes[node]['type'] != 'switch' else '' for node in network.nodes}
-
-    # Define the figure size
-    plt.figure(figsize=(6, 12))
-
-    # Draw nodes
-    node_size = [20 if network.nodes[node]['type'] == 'switch' else 200 for node in network.nodes]
-    node_color = ['red' if network.nodes[node]['type'] == 'switch' else 'lightblue' for node in network.nodes]
-    nx.draw_networkx_nodes(network, pos, node_size=node_size, node_color=node_color)
-
-    # Draw edges
-    #for edge in network.edges(data=True):
-    #    if edge[2]['type'] == 'track':
-    #        nx.draw_networkx_edges(network, pos, edgelist=[(edge[0], edge[1])], edge_color='gray')
-
-    #edge_labels = {(edge[0], edge[1]): edge[2]['length'] for edge in network.edges(data=True) if
-    #               edge[2]['type'] == 'track'}
-
-    # Draw edges
-    edge_labels = {}
-    edge_colors = []
-    for edge in network.edges(data=True):
-        if edge[2]['type'] == 'track':
-            source = edge[0]
-            target = edge[1]
-            edge_idx = list(network.edges(source, data=True)).index(edge)
-            offset = (edge_idx - len(network.edges(source)) / 2) * 0.1  # Adjust the offset value as needed
-            edge_labels[(source, target)] = edge[2]['length']
-            edge_colors.append('gray')
-            pos_source = pos[source]
-            pos_target = pos[target]
-            pos_edge = {
-                source: (pos_source[0], pos_source[1] + offset),
-                target: (pos_target[0], pos_target[1] + offset)
-            }
-            nx.draw_networkx_edges(network, pos_edge, edgelist=[(source, target)], edge_color='gray', width=1, connectionstyle='arc3, rad = 0.5')
-
-    # Draw labels
-    nx.draw_networkx_labels(network, pos, labels=labels, font_size=10, font_color='black')
-    #nx.draw_networkx_edge_labels(network, pos, edge_labels=edge_labels, font_size=8, font_color='red')
-
-    plt.title("Railway Network")
-    plt.axis('off')
-
-    #Save the figure
-    plt.savefig("railway_network.png")
-
-    plt.show()
-
-def visualize_new(network):
-    pos = nx.get_node_attributes(network, 'pos')
-    nx.draw(network, pos, with_labels=True, connectionstyle='arc3, rad = 0.1')
-    edge_labels = dict([((u, v,), d['length']) for u, v, d in network.edges(data=True)])
-
-    plt.show()
-
-
-def visualize(network):
-    # Create a graph to visualize your network
-    G = network
-
-    # Filter nodes based on the 'pos' and 'type' attributes
-    node_positions = {node: attributes.get('pos', (0, 0)) for node, attributes in G.nodes(data=True)}
-    node_types = {node: attributes.get('type', '') for node, attributes in G.nodes(data=True)}
-
-    # Create lists of nodes for each type
-    station_nodes = [node for node, node_type in node_types.items() if node_type == 'station']
-    switch_nodes = [node for node, node_type in node_types.items() if node_type == 'switch']
-    platform_nodes = [node for node, node_type in node_types.items() if node_type == 'platform']
-    vswitch_nodes = [node for node, node_type in node_types.items() if node_type == 'vswitch']
-
-    # Draw nodes based on type
-    plt.figure(figsize=(12, 6))
-
-    nx.draw(G, pos=node_positions, nodelist=station_nodes, node_color='skyblue', node_size=300, font_size=10,
-            node_shape='o', with_labels=True)
-    nx.draw(G, pos=node_positions, nodelist=switch_nodes, node_color='lightgray', node_size=300, font_size=10,
-            node_shape='o', with_labels=True)
-    nx.draw(G, pos=node_positions, nodelist=platform_nodes, node_color='lightgray', node_size=300, font_size=10,
-            node_shape='o', with_labels=True)
-    nx.draw(G, pos=node_positions, nodelist=vswitch_nodes, node_color='lightgray', node_size=300, font_size=10,
-            node_shape='o', with_labels=True)
-
-    # Draw edges without considering edge types
-    nx.draw_networkx_edges(G, pos=node_positions, edgelist=G.edges(), edge_color='gray')
-
-    # Add a legend for node types
-    legend_labels = {
-        'station': 'Stations',
-        'switch': 'Switches',
-        'platform': 'Platforms',
-        'vswitch': 'Virtual Switches',
-    }
-    for node_type, label in legend_labels.items():
-        plt.scatter([], [], label=label, c='lightgray', s=100)
-    plt.legend(scatterpoints=1, frameon=False, title='Node Types')
-
-    plt.title("Network Visualization")
-    plt.show()
-
 
 def check_type(network):
     default_node_type = 'unknown'
