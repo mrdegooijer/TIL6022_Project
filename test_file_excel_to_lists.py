0c7259f36b8e1d16f15f2870e21498f95ecc42d1
juliavanberkom <juliavanberkom18@gmail.com>
2023-10-31 15:15:13 +0100

    update excel name

--- test_file_excel_to_lists.py
+++ test_file_excel_to_lists.py
@@ -14,26 +14,26 @@ HS_Rdam_table = f'https://raw.githubusercontent.com/{username}/{repository}/{bra
 Rdam_HS_table = f'https://raw.githubusercontent.com/{username}/{repository}/{branch}/{filepath_Rdam_HS}'
 
 # Read Excel file into a DataFrame
-df_HS_Rdam = pd.read_excel(HS_Rdam_table)
-df_Rdam_HS = pd.read_excel(Rdam_HS_table)
+df_HS_Rdam = pd.read_excel(HS_Rdam_table, sheet_name='dep_times')
+df_Rdam_HS = pd.read_excel(Rdam_HS_table, sheet_name='dep_times')
 
 # Print the DataFrame
 print(df_HS_Rdam)
 
 # Reverse the order of columns in df_Rdam_HS
 df_Rdam_HS = df_Rdam_HS.iloc[:, ::-1]
-print(df_Rdam_HS)
+#print(df_Rdam_HS)
 
-df_HS_Rdam_sorted = df_HS_Rdam.sort_values(by=df_HS_Rdam.columns[0])
-df_Rdam_HS_sorted = df_Rdam_HS.sort_values(by=df_Rdam_HS.columns[7])
+#df_HS_Rdam_sorted = df_HS_Rdam.sort_values(by=df_HS_Rdam.columns[0])
+#df_Rdam_HS_sorted = df_Rdam_HS.sort_values(by=df_Rdam_HS.columns[7])
 
 # Concatenate the sorted DataFrames
-alltrains_df = pd.concat([df_HS_Rdam_sorted, df_Rdam_HS_sorted])
-alltrains_sorted = alltrains_df.sort_values(by=alltrains_df.columns[0])
+alltrains_df = pd.concat([df_HS_Rdam, df_Rdam_HS])
+alltrains_sorted = alltrains_df.sort_values(by=alltrains_df.columns[1])
 # Print the combined and sorted DataFrame
 print(alltrains_sorted)
 
-deptimes_HS = df_HS_Rdam[df_HS_Rdam.columns[0]].tolist()
+deptimes_HS = df_HS_Rdam[df_HS_Rdam.columns[1]].tolist()
 
 # Convert the list elements to datetime format
 deptimes_HS = pd.to_datetime(deptimes_HS, format='%H:%M:%S')
@@ -41,7 +41,7 @@ deptimes_HS = pd.to_datetime(deptimes_HS, format='%H:%M:%S')
 # Format the datetime objects as strings (HH:MM:SS)
 deptimes_HS = deptimes_HS.strftime('%H:%M:%S').tolist()
 
-traintype_HS = df_HS_Rdam[df_HS_Rdam.columns[8]].tolist()
+traintype_HS = df_HS_Rdam[df_HS_Rdam.columns[2]].tolist()
 
 # Print the list
 print(deptimes_HS)
