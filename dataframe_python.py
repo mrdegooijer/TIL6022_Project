dbf5d413a4797fcc2e9ac5e39c7fee63d458eb1c
tcdebruin <tanja.de.bruin@upcmail.nl>
2023-11-07 19:24:58 +0100

    git commit dataframe_python.py

--- dataframe_python.py
+++ dataframe_python.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import plotly.express as px
 
+# GitHub file URL
+username = 'mrdegooijer'
+repository = 'TIL6022_Project'
+branch = 'main'
+filepath_disruption_2021 = 'Data/disruptions-2021.csv'
+filepath_disruption_2022 = 'Data/disruptions-2022.csv'
 
+file_path1 = f'https://raw.githubusercontent.com/{username}/{repository}/{branch}/{filepath_disruption_2021}'
+file_path2 = f'https://raw.githubusercontent.com/{username}/{repository}/{branch}/{filepath_disruption_2022}'
 
-file_path1 = r'C:\Users\Gebruiker\Documents\Delft\TIL6022_Project\Data\disruptions-2021.csv'
-file_path2 = r'C:\Users\Gebruiker\Documents\Delft\TIL6022_Project\Data\disruptions-2022.csv'
 data1 = pd.read_csv(file_path1)
 data2 = pd.read_csv(file_path2)
 
 traject_2021 = data1[data1['rdt_lines_id'] == '11']
 traject_2022 = data2[data2['rdt_lines_id'] == '11']
-
-
-
 
 fig1 = px.pie(traject_2021, values='duration_minutes', names='cause_en')
 fig2 = px.pie(traject_2022, values='duration_minutes', names='cause_en')
